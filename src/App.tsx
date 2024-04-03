7ebd1faaa220c241b00b7cebd789e0f4d810c7b7
pierre-assamoi <p.assamoi@gmail.com>
2024-04-03 22:27:48 +0200

    jamais 2 sans 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './Components/Home'
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </>
   )
 }
