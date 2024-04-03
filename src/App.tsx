cc231f94761b9a6efda02568c711ecfb9ba9d879
pierre-assamoi <p.assamoi@gmail.com>
2024-04-04 00:05:56 +0200

    ALORS PEUT-ETRE

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import Home from './Components/Home'
+import { useState } from 'react'
 
 function App() {
+  const [counter, setCounter] = useState(0)
+
+
+  const changeCounter = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (e.currentTarget.value === '+') setCounter(prev => prev + 1)
+    if ((e.currentTarget.value === '-') && counter > 0) setCounter(prev => prev - 1)
+  }
+
   return (
     <>
-      <Routes>
-        <Route path={'/counter/'} element={<Home />} />
-      </Routes>
+      <div className=' h-screen flex flex-col bg-slate-600 items-center justify-center text-slate-200'>
+        <h1 className=' text-4xl font-bold'>Counter</h1>
+        <p className='text-2xl'>This is my standard counter app.</p>
+        <h1 className={counter > 10 ? `text-4xl` : 'text-2xl'}>{counter}</h1>
+        <div className="flex mt-4 w-24 items-center justify-around">
+          <button
+            className="px-4 py-2 border-2 border-slate-200 rounded h-[40px] w-[40px] flex items-center justify-center"
+            onClick={changeCounter}
+            value={'+'}>+</button>
+          <button
+            className="px-4 py-2 border-2 border-slate-200 rounded h-[40px] w-[40px] flex items-center justify-center"
+            onClick={changeCounter}
+            value={'-'}>-</button>
+        </div>
+      </div>
     </>
   )
 }
